dd124f2dd958d5a1ed7c3e8141ebbd4475b97a0d
Deepak Aggarwal <deepakaggarwal7@gmail.com>
2019-01-07 10:26:26 +0530

    npm init and boilerplate added

---
+++ src/components/SocialLogin.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import {View,Text} from 'react-native'
+
+export class SocialLogin implements Component{
+    render(){
+        return(
+            <View>
+                <Text>SocialLogin Component</Text>
+            </View>
+        )
+    }
+}
